751ff625fbe6438d16073090f97868843527008d
huang <1562262496@qq.com>
2019-11-19 16:33:51 +0800

    rain submit second

--- hello.c
+++ hello.c
@@ -4,4 +4,5 @@ int main(int arg, char* argv[]){
     printf("inited by SCM.\n");
     printf("develop branch created by root.\n");
     printf("rain change hello.c.\n");
+    printf("rain chenge second hello.c.\n");
 }
